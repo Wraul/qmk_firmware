dfc6bd5890f26946fcfbbe7cf9be2dc525bc63d3
Mathias Andersson <wraul@dbox.se>
2019-06-16 14:03:19 +0200

    Add windows versions of M_LCBR & M_RCBR

--- users/wraul/wraul.c
+++ users/wraul/wraul.c
@@ -1,148 +1,181 @@
 #include "keymap_swedish.h"
 #include "wraul.h"
 
-__attribute__((weak)) bool process_record_keymap(uint16_t keycode,
-                                                 keyrecord_t *record) {
-  return true;
-}
+__attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // Prints the version of the firmware.
-  case M_VRSN:
-    if (record->event.pressed) {
-      SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      return false;
-    }
-    break;
-    // Makes it easier to type { and [ on a Swedish version of Mac OS.
-    // Types a { and if combined with a shift it prints a [.
-  case M_LCBR:
-    if (record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
-      unregister_mods(temp_mod);
-      clear_oneshot_mods();
+    switch (keycode) {
+            // Prints the version of the firmware.
+        case M_VRSN:
+            if (record->event.pressed) {
+                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+                return false;
+            }
+            break;
+            // Makes it easier to type { and [ on a Swedish version of Mac OS.
+            // Types a { and if combined with a shift it prints a [.
+        case M_LCBR:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
 
-      if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
-        tap_code16(LALT(KC_8));
-      } else {
-        tap_code16(LSFT(LALT(KC_8)));
-      }
-      register_mods(temp_mod);
-      return false;
-    }
-    break;
-    // Makes it easier to type } and ] on a Swedish version of Mac OS.
-    // Types a } and if combined with a shift it prints a ].
-  case M_RCBR:
-    if (record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
-      unregister_mods(temp_mod);
-      clear_oneshot_mods();
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
+                    tap_code16(LALT(KC_8));
+                } else {
+                    tap_code16(LSFT(LALT(KC_8)));
+                }
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+            // Makes it easier to type } and ] on a Swedish version of Mac OS.
+            // Types a } and if combined with a shift it prints a ].
+        case M_RCBR:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
 
-      if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
-        tap_code16(LALT(KC_9));
-      } else {
-        tap_code16(LSFT(LALT(KC_9)));
-      }
-      register_mods(temp_mod);
-      return false;
-    }
-    break;
-  // Makes it easier to type ~ and ` on a Swedish version of Mac OS.
-  // Types a ~ followed by a space to take us out of the combine character mode.
-  // If combined with a shift it does the same but prints a `.
-  case M_CIRC:
-    if (record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
-      unregister_mods(temp_mod);
-      clear_oneshot_mods();
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
+                    tap_code16(LALT(KC_9));
+                } else {
+                    tap_code16(LSFT(LALT(KC_9)));
+                }
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+            // Makes it easier to type { and [ on a Swedish version of Windows.
+            // Types a { and if combined with a shift it prints a [.
+        case M_LCBR_W:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
 
-      if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
-        tap_code16(LSFT(NO_ACUT));
-        tap_code(KC_SPC);
-      } else {
-        tap_code16(LALT(NO_QUOT));
-        tap_code(KC_SPC);
-      }
-      register_mods(temp_mod);
-      return false;
-    }
-    break;
-  // Compiles the firmware, and adds the flash command based on keyboard
-  // bootloader
-  case M_MAKE:
-    if (!record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
-      unregister_mods(temp_mod);
-      clear_oneshot_mods();
-      SEND_STRING("make " QMK_KEYBOARD ":" QMK_KEYMAP);
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
+                    tap_code16(LALT(KC_7));
+                } else {
+                    tap_code16(ALGR(KC_7));
+                }
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+            // Makes it easier to type } and ] on a Swedish version of Windows.
+            // Types a } and if combined with a shift it prints a ].
+        case M_RCBR_W:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
+
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
+                    tap_code16(LALT(KC_0));
+                } else {
+                    tap_code16(ALGR(KC_0));
+                }
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+        // Makes it easier to type ~ and ` on a Swedish version of Mac OS.
+        // Types a ~ followed by a space to take us out of the combine character mode.
+        // If combined with a shift it does the same but prints a `.
+        case M_CIRC:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
+
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT) {
+                    tap_code16(LSFT(NO_ACUT));
+                    tap_code(KC_SPC);
+                } else {
+                    tap_code16(LALT(NO_QUOT));
+                    tap_code(KC_SPC);
+                }
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+        // Compiles the firmware, and adds the flash command based on keyboard
+        // bootloader
+        case M_MAKE:
+            if (!record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
+                unregister_mods(temp_mod);
+                clear_oneshot_mods();
+                SEND_STRING("make " QMK_KEYBOARD ":" QMK_KEYMAP);
 #ifndef FLASH_BOOTLOADER
-      if ((temp_mod | temp_osm) & MOD_MASK_SHIFT)
+                if ((temp_mod | temp_osm) & MOD_MASK_SHIFT)
 #endif
-      {
-#if defined(__arm__) // only run for ARM boards
-        SEND_STRING(":dfu-util");
-#elif defined(BOOTLOADER_DFU)      // only run for DFU boards
-        SEND_STRING(":dfu");
-#elif defined(BOOTLOADER_HALFKAY)  // only run for teensy boards
-        SEND_STRING(":teensy");
-#elif defined(BOOTLOADER_CATERINA) // only run for Pro Micros
-        SEND_STRING(":avrdude");
-#endif                             // bootloader options
-      }
-      if ((temp_mod | temp_osm) & MOD_MASK_CTRL) {
-        SEND_STRING(" -j8 --output-sync");
-      }
-      SEND_STRING(SS_TAP(X_ENTER));
-      register_mods(temp_mod);
-      return false;
-    }
-    break;
-  // Creates a key that acts like , but types a < if combined with alt on a
-  // Swedish version of Mac OS. Note that this requires that the keyboard is set
-  // to ISO in the keyboard settings pane. Otherwise it appears that the NUBS
-  // key is incorrectly interpreted as another key.
-  case M_LBRC:
-    if (record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
+                {
+#if defined(__arm__)  // only run for ARM boards
+                    SEND_STRING(":dfu-util");
+#elif defined(BOOTLOADER_DFU)       // only run for DFU boards
+                    SEND_STRING(":dfu");
+#elif defined(BOOTLOADER_HALFKAY)   // only run for teensy boards
+                    SEND_STRING(":teensy");
+#elif defined(BOOTLOADER_CATERINA)  // only run for Pro Micros
+                    SEND_STRING(":avrdude");
+#endif                              // bootloader options
+                }
+                if ((temp_mod | temp_osm) & MOD_MASK_CTRL) {
+                    SEND_STRING(" -j8 --output-sync");
+                }
+                SEND_STRING(SS_TAP(X_ENTER));
+                register_mods(temp_mod);
+                return false;
+            }
+            break;
+        // Creates a key that acts like , but types a < if combined with alt on a
+        // Swedish version of Mac OS. Note that this requires that the keyboard is set
+        // to ISO in the keyboard settings pane. Otherwise it appears that the NUBS
+        // key is incorrectly interpreted as another key.
+        case M_LBRC:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
 
-      if ((temp_mod | temp_osm) & MOD_MASK_ALT) {
-        unregister_mods(temp_mod);
-        clear_oneshot_mods();
-        tap_code(KC_NUBS);
-        register_mods(temp_mod);
-      } else {
-        tap_code(KC_COMM);
-      }
-      return false;
-    }
-    break;
-  // Creates a key that acts like . but types a > if combined with alt on a
-  // Swedish version of Mac OS. Note that this requires that the keyboard is set
-  // to ISO in the keyboard settings pane. Otherwise it appears that the NUBS
-  // key is incorrectly interpreted as another key.
-  case M_RBRC:
-    if (record->event.pressed) {
-      uint8_t temp_mod = get_mods();
-      uint8_t temp_osm = get_oneshot_mods();
+                if ((temp_mod | temp_osm) & MOD_MASK_ALT) {
+                    unregister_mods(temp_mod);
+                    clear_oneshot_mods();
+                    tap_code(KC_NUBS);
+                    register_mods(temp_mod);
+                } else {
+                    tap_code(KC_COMM);
+                }
+                return false;
+            }
+            break;
+        // Creates a key that acts like . but types a > if combined with alt on a
+        // Swedish version of Mac OS. Note that this requires that the keyboard is set
+        // to ISO in the keyboard settings pane. Otherwise it appears that the NUBS
+        // key is incorrectly interpreted as another key.
+        case M_RBRC:
+            if (record->event.pressed) {
+                uint8_t temp_mod = get_mods();
+                uint8_t temp_osm = get_oneshot_mods();
 
-      if ((temp_mod | temp_osm) & MOD_MASK_ALT) {
-        unregister_mods(temp_mod);
-        clear_oneshot_mods();
-        tap_code16(LSFT(KC_NUBS));
-        register_mods(temp_mod);
-      } else {
-        tap_code(KC_DOT);
-      }
-      return false;
+                if ((temp_mod | temp_osm) & MOD_MASK_ALT) {
+                    unregister_mods(temp_mod);
+                    clear_oneshot_mods();
+                    tap_code16(LSFT(KC_NUBS));
+                    register_mods(temp_mod);
+                } else {
+                    tap_code(KC_DOT);
+                }
+                return false;
+            }
+            break;
     }
-    break;
-  }
-  return process_record_keymap(keycode, record);
+    return process_record_keymap(keycode, record);
 }
--- users/wraul/wraul.h
+++ users/wraul/wraul.h
@@ -5,15 +5,17 @@
 #include "version.h"
 
 enum custom_keycodes {
-  M_MAKE = SAFE_RANGE,
-  M_VRSN,
-  M_LCBR,
-  M_RCBR,
-  M_PIPE,
-  M_CIRC,
-  M_LBRC,
-  M_RBRC,
-  KEYMAP_SAFE_RANGE  // use "KEYMAP_SAFE_RANGE" for keymap specific codes
+    M_MAKE = SAFE_RANGE,
+    M_VRSN,
+    M_LCBR,
+    M_RCBR,
+    M_LCBR_W,
+    M_RCBR_W,
+    M_PIPE,
+    M_CIRC,
+    M_LBRC,
+    M_RBRC,
+    KEYMAP_SAFE_RANGE  // use "KEYMAP_SAFE_RANGE" for keymap specific codes
 };
 
 // Swap space in Mac OS
